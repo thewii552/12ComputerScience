f6605f3e66df0ab39e9127b3104b77261007a65d
andr6491 <andr6491@ISS147D8-27.scdsb.pri>
2015-12-07 10:40:24 -0500

    Finished class content, needs icons

--- Unit 4-Exploring Advanced Algorithms/Unit4/src/Projects/PlayersGUIExtra.java
+++ Unit 4-Exploring Advanced Algorithms/Unit4/src/Projects/PlayersGUIExtra.java
@@ -16,7 +16,7 @@ import javax.swing.DefaultListModel;
  *
  * @author andr6491
  */
-public class PlayersGUI extends javax.swing.JFrame {
+public class PlayersGUIExtra extends javax.swing.JFrame {
 
     ArrayList<Player> players = new ArrayList();
     DefaultListModel model = new DefaultListModel();
@@ -24,7 +24,7 @@ public class PlayersGUI extends javax.swing.JFrame {
     /**
      * Creates new form PlayersGUI
      */
-    public PlayersGUI() {
+    public PlayersGUIExtra() {
         initComponents();
         
         //Load the players
@@ -263,20 +263,20 @@ public class PlayersGUI extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(PlayersGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PlayersGUIExtra.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(PlayersGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PlayersGUIExtra.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(PlayersGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PlayersGUIExtra.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(PlayersGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PlayersGUIExtra.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new PlayersGUI().setVisible(true);
+                new PlayersGUIExtra().setVisible(true);
             }
         });
     }
